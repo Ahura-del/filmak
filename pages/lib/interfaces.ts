230884e6aed99c00e199edcf4141d0a694a69747
ahura <ahuradel@gmail.com>
2023-01-10 16:18:44 +0330

    create interface file for app interfaces and types

---
+++ pages/lib/interfaces.ts
@@ -0,0 +1,14 @@
+export interface DataType {
+    item :{
+
+        country:string;
+        genres:string[];
+    id:number;
+    images:string[];
+    poster:string;
+    title:string;
+    year:string;
+    imdb_rating:string
+}
+  }
+  
